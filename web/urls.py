ca05595973cfc1b305cd750e71251281dc9accd4
Mateus Gundel <mateus8923@gmail.com>
2019-05-08 01:51:39 -0300

    Adicionado template inicial do trabalho

---
+++ web/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from web import views
+
+urlpatterns = [
+    path('home/', views.home_view),
+]
--- web/views.py
+++ web/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def home_view(request):
+    data = {}
+    data.update({'var': 'O app funcionou'})
+    return render(request, 'web/home.html', data)
