421d9c53b68851e638dfefd8bfadfad01de1acd7
Mateus Gundel <mateus8923@gmail.com>
2019-05-13 01:09:10 -0300

    Adicionado arquvios do bootstrap

--- config/settings.py
+++ config/settings.py
@@ -72,16 +72,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'reconhecedor',
-        'USER': 'mateus',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
