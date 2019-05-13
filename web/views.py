5bef7c82a8e461a2f498bd34fb0692e9c4348f0d
Mateus Gundel <mateus8923@gmail.com>
2019-05-12 22:07:19 -0300

    Feito a parte do front-end

--- web/views.py
+++ web/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import render, HttpResponse, Http404
+import json
 
 
 # Create your views here.
 def home_view(request):
-    data = {}
-    data.update({'var': 'O app funcionou'})
-    return render(request, 'web/home.html', data)
+    return render(request, 'web/home.html')
+
+
+@csrf_exempt
+def create_post(request):
+    if request.method == 'POST':
+        print(request.POST)
+        response_data = request.POST
+        return HttpResponse(
+            json.dumps(response_data),
+            content_type="application/json"
+        )
+    else:
+        raise Http404
