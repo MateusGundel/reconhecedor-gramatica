ada8889f0cb49d555e93705ddf971b4bcb2a52cd
Mateus Gundel <mateus8923@gmail.com>
2019-05-22 05:04:05 -0300

    [FIX] Alterado o numero de iterações máximas

--- daemon/sentencas.py
+++ daemon/sentencas.py
@@ -25,7 +25,7 @@ def get_uma_sentenca(object_from_view, result_list):
     regex = re.compile("[A-Z]")
 
     continuar = 0
-    while continuar < 10:
+    while continuar < 20:
         final_result = ""
         foi_uma_vez = False
         for item in initial_result:
@@ -54,7 +54,7 @@ def get_result_production(object, finder, iteractions, regex):
         for item in str(production['esquerda']).replace(" ", "").split("|"):
             if finder is item:
                 if len(production_direita) > 1:
-                    if (iteractions < 5):
+                    if (iteractions < 10):
                         return str(production_direita[random.randrange(0, len(production_direita))])
                     else:
                         for retorno in production_direita:
