67bf31deacd4eb54d59ec956998d5f58f7198d43
Mateus Gundel <mateus8923@gmail.com>
2019-07-03 00:33:26 -0300

    [ADD] Adicionado dict para o processamento

--- daemon/transformar/transformar_glc.py
+++ daemon/transformar/transformar_glc.py
@@ -1,7 +1,12 @@
 def __start__(object_from_view):
-    pass
-def elimina_producoes_vazias():
-    pass
+    producoes = {}
+    for prod in object_from_view["producao"]:
+        producoes.update({prod["esquerda"]:prod["direita"].split("|")})
+    elimina_producoes_vazias(producoes)
+
+def elimina_producoes_vazias(producoes):
+    for producao in producoes:
+        print(str(producao) + " : "+str(producoes[producao]))
 def elimina_producoes_unitarias():
     pass
 def elimina_simbolos_inuteis():
