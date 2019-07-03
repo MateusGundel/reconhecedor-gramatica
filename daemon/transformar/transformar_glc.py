ca7d52c8646c15984f9e8a14c685942ac1a071fc
ThalesMuller <thalesem@mx2.unisc.br>
2019-07-03 01:40:32 -0300

    [ADD] implementação da função elimina_producoes_vazias

--- daemon/transformar/transformar_glc.py
+++ daemon/transformar/transformar_glc.py
@@ -1,33 +1,33 @@
 class transformation:
     count_nao_terminal = 0
     sentenca_vazia = "&"
+    producoes = []
 
     def __init__(self, object_from_view):
         producoes = {}
         for prod in object_from_view["producao"]:
             producoes.update({prod["esquerda"]: prod["direita"].split("|")})
-        self.elimina_producoes_vazias(producoes)
+        self.elimina_producoes_vazias()
         self.recursao_a_esquerda(producoes)
 
-    def elimina_producoes_vazias(self, producoes):
+    def elimina_producoes_vazias(self):
         conjunto_var_leva_vazio = []
-        for producao in producoes:
-            if (self.sentenca_vazia in producoes[producao]):
+        for producao in self.producoes:
+            if (self.sentenca_vazia in self.producoes[producao]):
                 conjunto_var_leva_vazio.append(producao)
-                producoes[producao] = producoes[producao][producoes[producao].index(
-                    self.sentenca_vazia)] = producao
+                self.producoes[producao] = self.producoes[producao][self.producoes[producao].index(self.sentenca_vazia)] = self.producao
         if(len(conjunto_var_leva_vazio > 0)):
-            for producao in producoes[producao]:
-                if conjunto_var_leva_vazio[0] in producoes[producao]:
+            for producao in self.producoes[producao]:
+                if conjunto_var_leva_vazio[0] in self.producoes[producao]:
                     conjunto_var_leva_vazio.append(producao)
-            for producao in producoes:
-                for index, elemento in enumerate(producoes[producao]):
+            for producao in self.producoes:
+                for index, elemento in enumerate(self.producoes[producao]):
                     for char in elemento:
                         if (char in conjunto_var_leva_vazio):
-                            producoes[producao][index].append(elemento.replace(char, ""))
+                            self.producoes[producao][index].append(elemento.replace(char, ""))
+        
 
     def elimina_producoes_unitarias(self, producoes):
-
         pass
 
     def elimina_simbolos_inuteis(self, producoes):
