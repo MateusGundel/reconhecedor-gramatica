c2f9f26e271b837e52bc8604bb34095f2e21810c
ThalesMuller <thalesem@mx2.unisc.br>
2019-07-02 23:27:08 -0300

    [ADD] adcionadas as declarações das principais funcções

--- daemon/transformar/transformar_glc.py
+++ daemon/transformar/transformar_glc.py
@@ -1,2 +1,14 @@
 def __start__(object_from_view):
     pass
+def elimina_producoes_vazias():
+    pass
+def elimina_producoes_unitarias():
+    pass
+def elimina_simbolos_inuteis():
+    pass
+def fatoracao():
+    pass
+def recursao_a_esquerda():
+    pass
+def resolve_ambiguidade(): #opcional, nem fudendo q a gente vai fazer
+    pass
