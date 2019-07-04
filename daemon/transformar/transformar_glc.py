711078332dfae4ffd3ab4bbbd4ba3baeac7a78b1
Mateus Gundel <mateus8923@gmail.com>
2019-07-03 21:23:21 -0300

    [FIX] Arrumado recursão a esquerda

--- daemon/transformar/transformar_glc.py
+++ daemon/transformar/transformar_glc.py
@@ -14,16 +14,16 @@ class transformation:
         self.gramatica_inicial = object_from_view["gramatica-inicial"]
         for prod in object_from_view["producao"]:
             self.producoes.update({prod["esquerda"]: prod["direita"].replace(" ", "").split("|")})
-        #print(self.producoes)
+        # print(self.producoes)
         self.elimina_producoes_vazias()
-        #print(self.producoes)
+        # print(self.producoes)
         self.elimina_producoes_unitarias()
-        #print(self.producoes)
+        # print(self.producoes)
         self.elimina_simbolos_inuteis()
-        #print(self.producoes)
-        #self.fatoracao()
-        #print(self.producoes)
-        #self.recursao_a_esquerda()
+        # print(self.producoes)
+        # self.fatoracao()
+        print(self.producoes)
+        self.recursao_a_esquerda()
         print(self.producoes)
 
     def elimina_producoes_vazias(self):
@@ -32,7 +32,7 @@ class transformation:
             if (self.sentenca_vazia in self.producoes[producao]):
                 conjunto_var_leva_vazio.append(producao)
                 self.producoes[producao].remove(self.sentenca_vazia)
-        if(len(conjunto_var_leva_vazio) > 0):
+        if (len(conjunto_var_leva_vazio) > 0):
             for producao in self.producoes:
                 if (conjunto_var_leva_vazio[0] in self.producoes[producao]):
                     conjunto_var_leva_vazio.append(producao)
@@ -41,14 +41,14 @@ class transformation:
                     for char in elemento:
                         if (char in conjunto_var_leva_vazio):
                             self.producoes[producao].append(elemento.replace(char, ""))
-        
+
     def elimina_producoes_unitarias(self):
         for producao in self.producoes:
             for index, elemento in enumerate(self.producoes[producao]):
-                if((len(elemento) == 1) and (elemento in self.nao_terminais)):
+                if ((len(elemento) == 1) and (elemento in self.nao_terminais)):
                     del self.producoes[producao][index]
                     for producao_var_unica in self.producoes[elemento]:
-                        if(not(producao_var_unica in self.producoes[producao])):
+                        if (not (producao_var_unica in self.producoes[producao])):
                             self.producoes[producao].append(producao_var_unica)
 
     def elimina_simbolos_inuteis(self):
@@ -61,7 +61,7 @@ class transformation:
                     for node in self.producoes[producao]:
                         if (elemento in node):
                             is_in_a_prod = True
-            if(not(is_in_a_prod)):
+            if (not (is_in_a_prod)):
                 del self.producoes[elemento]
 
     def fatoracao(self):
@@ -79,11 +79,9 @@ class transformation:
                     # print("B-------------- "+str(item["letter"])+ "  "+str(item["word"])+"  "+ str(item["producao"]))
                     # print("2--------- "+str(item2["letter"])+ "  "+str(item2["word"])+"  "+ str(item2["producao"]))
 
-
     def recursao_a_esquerda(self):
         producoes = self.producoes
         list_recursoes = []
-        new_variable = None
         for producao in producoes:
             for word in producoes[producao]:
                 for letter in word:
@@ -92,19 +90,21 @@ class transformation:
                             for word2 in producoes[prod2]:
                                 for letter2 in word2:
                                     if letter2 == letter and letter2 != word2:
-                                        list_recursoes.append((prod2,producao))
+                                        list_recursoes.append((prod2, producao))
 
         for recursao in list_recursoes:
             for index, producao in enumerate(producoes[recursao[0]]):
                 if recursao[1] in producao:
-                    for p in producoes[recursao[1]]:
-                        producoes[recursao[0]].pop(index)
-                        new_variable = str(self.get_non_terminal_value())
-                        producoes.update({new_variable:[(p+producao.replace(recursao[1], ""))
-                                         .replace(recursao[0], "")+new_variable, "&"]})
-        if new_variable is not None:
-            for index, producao in enumerate(producoes[recursao[0]]):
-                producoes[recursao[0]][index] = producoes[recursao[0]][index]+new_variable
+                    #remove a recursão
+                    producoes[recursao[0]].pop(index)
+                    # define nova produção e seta utilizando o que sobrou junto da recursão
+                    new_variable = str(self.get_non_terminal_value())
+                    producoes.update({new_variable: [
+                        (producao.replace(recursao[1], "")).replace(recursao[0], "") + new_variable, "&"]})
+                    #adiciona novo não terminal no final
+                    for index, producao in enumerate(producoes[recursao[0]]):
+                        producoes[recursao[0]][index] = producoes[recursao[0]][index] + new_variable
+
         self.producoes = producoes
 
     def resolve_ambiguidade(self):  # opcional, nem fudendo q a gente vai fazer
